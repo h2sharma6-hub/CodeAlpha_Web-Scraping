d3a0eaec6d7bf2cb70a2846aec95a15829751856
Himanshu Sharma <h2sharma6@gmail.com>
2025-08-21 15:03:20 +0530

    Add files via upload

---
+++ CodeAlpha_task1.py
@@ -0,0 +1,31 @@
+# Library import
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Website from where data is collected
+url = "http://quotes.toscrape.com/"
+response = requests.get(url)
+
+# Parsing the HTML
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Lists for storing data
+quotes = []
+authors = []
+
+# Extracting quotes
+for q in soup.find_all("span", class_="text"):
+    quotes.append(q.text)
+
+# Extracting authors
+for a in soup.find_all("small", class_="author"):
+    authors.append(a.text)
+
+# Creating DataFrame
+df = pd.DataFrame({"Quote": quotes, "Author": authors})
+
+# Saving dataset
+df.to_csv("quotes.csv", index=False)
+print("Quotes dataset ban gaya. Total records:", len(df))
+print(df.head())
